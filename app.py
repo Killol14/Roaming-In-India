cf592ad3d0ab2ad81b05c68daf9564190b33e146
Killol14 <kgselectrical.plumbing@gmail.com>
2023-05-11 11:36:22 +0000

    this will commit place.html

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-import os   
-from flask import Flask
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+from typing import Union
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__) 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "hello"
+@app.route("/get_places")
+def get_places():
+    places = mongo.db.places.find()
+    return render_template("places.html", places=places)
 
 
 if __name__ == "__main__":
