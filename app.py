dcdcd5c66ff79bae61bb549793d5cc88dcbd9480
Killol14 <kgselectrical.plumbing@gmail.com>
2023-05-11 15:07:47 +0000

    this will commit added account form and profile in place

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+def is_logged_in() -> Union[str, None]:
+    """
+    Returns None if the user isn't logged in otherwise returns the username
+    
+    """
+    return session.get("user")
+
 @app.route("/")
 @app.route("/get_places")
 def get_places():
@@ -46,6 +53,54 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Sign Up Successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")
+            ):
+                session["user"] = request.form.get("username").lower()
+                flash("Hi, {}".format(request.form.get("username")))
+                return redirect(url_for("account", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if is_logged_in():
+        """
+        (Chat GPT)
+        grab the session user's username from db
+        """
+        user = mongo.db.users.find_one({"username": session["user"]})
+        recipes = list(mongo.db.recipes.find({"created_by": user["username"]}))
+        return render_template("account.html", user=user, places=places)
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if is_logged_in():
+        # remove user from session cookie
+        flash("You have been logged out, see you soon!")
+        session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
