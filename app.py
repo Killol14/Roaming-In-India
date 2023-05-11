d9945fc4e5fb56a533f32889e75528dfe1a3bcbf
Killol14 <kgselectrical.plumbing@gmail.com>
2023-05-11 13:45:52 +0000

    this will commit sign up functionality

--- app.py
+++ app.py
@@ -24,10 +24,29 @@ def get_places():
     places = mongo.db.places.find()
     return render_template("places.html", places=places)
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up Successful!")
     return render_template("register.html")
-    
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
