cc1c28e94e57a31a4f9353046abc7282b0579d2c
Killol14 <kgselectrical.plumbing@gmail.com>
2023-05-17 17:11:19 +0000

    commit for import flask

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from bson.errors import InvalidId
-from typing import Union
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
